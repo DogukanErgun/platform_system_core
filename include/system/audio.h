93781140d3d2205d9ee80fdbf79d1e502e565daa
Shreyas Nagasandra Chandrasekhar <snagas@codeaurora.org>
2014-10-29 10:58:52 +0530

    audio: Changes for LPA/Tunnel mode
     Intitial changes to enable LPA/Tunnel mode
    
    Change-Id: I099480931661c1190b53363d2ee6716a78b81ccb
    
    Conflicts:
    	include/system/audio.h

--- include/system/audio.h
+++ include/system/audio.h
@@ -782,7 +782,12 @@ typedef enum {
     AUDIO_OUTPUT_FLAG_VOIP_RX = 0x1000,  // use this flag in combination with DIRECT to
                                          // indicate HAL to activate EC & NS
                                          // path for VOIP calls
-    AUDIO_OUTPUT_FLAG_INCALL_MUSIC = 0x2000 //use this flag for incall music delivery
+    AUDIO_OUTPUT_FLAG_INCALL_MUSIC = 0x2000, //use this flag for incall music delivery
+    // flag for HDMI compressed passthrough
+    AUDIO_OUTPUT_FLAG_COMPRESS_PASSTHROUGH = 0x4000,
+    AUDIO_OUTPUT_FLAG_LPA = 0x8000,
+    AUDIO_OUTPUT_FLAG_TUNNEL = 0x10000
+#endif
 } audio_output_flags_t;
 
 /* The audio input flags are analogous to audio output flags.
